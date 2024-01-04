76a956956c9768fa16967a0b648c3fd00eb857bc
Joshua Obaji <joshuaemeka2022@gmail.com>
2024-01-04 12:16:17 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -11,11 +11,8 @@ if(!isset($_SESSION['username'])){
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <title>Home page</title>
@@ -23,26 +20,20 @@ if(!isset($_SESSION['username'])){
   <body>
    <h1 class="text-center text-success mt-5" >welcome <?php echo $_SESSION['username']; ?></h1>
    <?php
-        include "include/connection.php"; // Include your database connection file
+       // database connection file
+        include "include/connection.php"; 
 
-                // Check if the user is logged in
-                // session_start();
-                // if (!isset($_SESSION['username'])) {
-                //     // Redirect to the login page if not logged in
-                //     header("Location: enter.php");
-                //     exit();
-                // }
 
-                // Fetch the username of the logged-in user
+                // Fetched the username of the logged-in user
                 $logged_in_username = $_SESSION['username'];
 
-                // Fetch user details from the database for the logged-in user
+                // Fetched user details from the database for the logged-in user
                 $query = "SELECT firstname, lastname, email FROM rexregister WHERE username = '$logged_in_username'";
                 $result = mysqli_query($con, $query);
 
-                // Check if the query was successful
+                // this codes checks if the query was successful
                 if ($result) {
-                    // Check if there is a row in the result set
+                    // Checking if there is a row in the result set
                     if ($row = mysqli_fetch_assoc($result)) {
                       echo '<table class="table table-bordered">
                       <thead>
@@ -69,7 +60,7 @@ if(!isset($_SESSION['username'])){
                     die(mysqli_error($con));
                 }
 
-// Close the database connection
+// Closing the database connection
 mysqli_close($con);
 ?>
 
