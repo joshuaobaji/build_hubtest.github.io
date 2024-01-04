667d34184ce05a93567a0fca638791ddea0d0615
Joshua Obaji <joshuaemeka2022@gmail.com>
2024-01-04 12:02:52 +0100

    Update connection.php

--- include/connection.php
+++ include/connection.php
@@ -1,12 +1,13 @@
 <?php
+// this code configures the database conection
 $HOSTNAME='localhost';
 $USERNAME='root';
 $PASSWORD= '';
 $DATABASE='rex';
 
-
+// establish a database connection
 $con=mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABASE);
-
+// check if the connection was succesfull
 if(!$con) {
      die(mysqli_connect($con)); 
 } 
