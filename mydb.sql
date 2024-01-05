677dfb21a498c490ea8c6483552ab387e458d320
Joshua Obaji <joshuaemeka2022@gmail.com>
2024-01-05 01:14:09 +0100

    Create mydb.sql

---
+++ mydb.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIME,
+    PRIMARY KEY (id)
+);
